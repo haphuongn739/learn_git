c0a1791fa4fbf32c334f2e1241a54e61c5c0f54a
HaPhuong-PC\Ha Phuong <haphuongn739@gmail.com>
2018-04-04 22:30:51 +0700

    edit post

---
+++ homework/homework/Exercise.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int luaChon = 0;
+
+void ShowTopMenu()
+{
+	cout <<" Xin chao, vui long chon chuc nang: " << endl;
+	cout <<" 1. Nhap SV: " << endl;
+	cout <<" 2. Xuat thong tin SV: " << endl;
+	cout <<" 3. Thoat" << endl;
+
+	cout <<"Lua chon cua ban: ";
+	cin >>luaChon;
+}
+
+void clearScreen()
+{
+	system("cls");
+}
+
+void inputSinhVien()
+{
+	char mssv[10];
+	cout <<"MSSV: ";
+	cin >>mssv;
+}
+
+void main()
+{
+ShowTopMenu:
+	ShowTopMenu();
+
+	clearScreen();
+	switch (luaChon)
+	{
+	case 1:
+		inputSinhVien();
+		break;
+	case 2:
+		cout <<"Display screen Output SV" <<endl;
+		break;
+	case 3:
+		cout <<"Exit" <<endl;
+		break;
+	}
+	cout << "Nhan phim 0 de tro ve man hinh chinh :" <<endl;
+	char x = getch();
+	if( x == '0' )
+	{
+		system ("cls");
+		goto ShowTopMenu;
+	}
+	system("pause");
+}
